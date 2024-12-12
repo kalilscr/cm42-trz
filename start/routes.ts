bced5407724b2f24e63b869601296e2b5c67152b
Rodrigo Kalil <rkbuarque@gmail.com>
2024-12-12 12:33:54 -0300

    feat: update routes

--- start/routes.ts
+++ start/routes.ts
@@ -8,9 +8,16 @@
 */
 
 import router from '@adonisjs/core/services/router'
+const ItemsPointsController = () => import('../app/controllers/items_points_controller.ts')
+const SurvivorsController = () => import('../app/controllers/survivors_controller.ts')
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
-})
+// survivors Controller
+router.post('survivor/create', [SurvivorsController, 'create'])
+router.put('survivor/:name/update-location', [SurvivorsController, 'updateLocation'])
+router.put('survivor/:id/report-contamination', [SurvivorsController, 'reportContamination'])
+
+// items points Controller
+router.post('item/point/create', [ItemsPointsController, 'create'])
+router.put('item/point/update', [ItemsPointsController, 'update'])
+router.get('item/point/list', [ItemsPointsController, 'list'])
+router.delete('item/point/delete', [ItemsPointsController, 'delete'])
