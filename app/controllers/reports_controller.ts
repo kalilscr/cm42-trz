16025436d3804caea33e9f62d6cb9a04dc1eb8eb
Rodrigo Kalil <rkbuarque@gmail.com>
2024-12-24 02:05:09 -0300

    feat: add reports controller

--- app/controllers/reports_controller.ts
+++ app/controllers/reports_controller.ts
@@ -1,4 +1,23 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import { inject } from '@adonisjs/core'
+import ReportsService from '../services/reports_service.ts'
 
+@inject()
 export default class ReportsController {
+  constructor(protected reportsService: ReportsService) {}
+
+  async getInfectedSurvivors() {
+    return await this.reportsService.percentageInfectedSurvivors()
+  }
+
+  async getNonInfectedSurvivors() {
+    return await this.reportsService.percentageNonInfectedSurvivors()
+  }
+
+  async getAverageResourcesPerSurvivor() {
+    return await this.reportsService.averageResourcesPerSurvivor()
+  }
+
+  async getLostPoints() {
+    return await this.reportsService.infectedSurvivorLostPoints()
+  }
 }
