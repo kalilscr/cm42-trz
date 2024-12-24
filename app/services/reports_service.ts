52d9a31138038237330f9c8213deb8840c94e01f
Rodrigo Kalil <rkbuarque@gmail.com>
2024-12-24 02:06:25 -0300

    feat: finish reports service

--- app/services/reports_service.ts
+++ app/services/reports_service.ts
@@ -1,7 +1,11 @@
+import { inject } from '@adonisjs/core'
+import ItemPoints from '../models/items_points.ts'
 import Survivor from '../models/survivors.ts'
+import TradeService from './trades_service.ts'
 
+@inject()
 export default class ReportsService {
-  constructor() {}
+  constructor(protected tradeService: TradeService) {}
 
   async percentageInfectedSurvivors() {
     //const infected = await Survivor.findManyBy({ isInfected: true })
@@ -23,7 +27,7 @@ export default class ReportsService {
     })
 
     const infectedSurvivorsPercentage = (100 * infectedCount) / totalCount
-    return infectedSurvivorsPercentage //.toFixed(2)
+    return `Infected survivors percentage is ${infectedSurvivorsPercentage}` //.toFixed(2)
   }
 
   async percentageNonInfectedSurvivors() {
@@ -41,10 +45,54 @@ export default class ReportsService {
     })
 
     const nonInfectedSurvivorsPercentage = (100 * nonInfectedCount) / totalCount
-    return nonInfectedSurvivorsPercentage
+    return `Not infected survivors percentage is ${nonInfectedSurvivorsPercentage}`
   }
 
-  async averageResourcesBySurvivor() {}
+  async averageResourcesPerSurvivor() {
+    const allSurvivors = await Survivor.query().preload('items')
 
-  async infectedSurvivorLostPoints() {}
+    // Sum up all resources
+    const totalResources = allSurvivors.reduce(
+      (acc, survivor) => {
+        survivor.items.forEach((item) => {
+          acc[item.name] = (acc[item.name] || 0) + item.quantity
+        })
+        return acc
+      },
+      {} as Record<string, number>
+    )
+
+    // Calculate averages
+    const averages: Record<string, number> = {}
+    Object.entries(totalResources).forEach(([itemName, total]) => {
+      // Round to 2 decimal places
+      averages[itemName] = Number((total / allSurvivors.length).toFixed(2))
+    })
+
+    return `Average amount of resources per survivor is: ${JSON.stringify(averages, null, 2)}`
+    //return averages
+  }
+
+  async infectedSurvivorLostPoints() {
+    const infected = await Survivor.query().preload('items').where('isInfected', 'true')
+
+    if (!infected.length) {
+      return `No infected survivors was found. Total points lost is: 0`
+    }
+
+    const itemsPoints = await ItemPoints.all()
+
+    const itemPointMap = new Map(itemsPoints.map((item) => [item.name, item.points]))
+
+    const pointsPerSurvivor = infected.map((survivor) => {
+      return this.tradeService.calculateTotalPoints(survivor.items, itemPointMap)
+    })
+
+    const total = pointsPerSurvivor.reduce((totalCount, points) => {
+      totalCount += points
+      return totalCount
+    })
+
+    return `Total points lost because of infected survivors is: ${total}`
+  }
 }
