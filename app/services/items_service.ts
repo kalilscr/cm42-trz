d421b2e72265a32e83b097e8d822c9ab5fb7f0b2
Rodrigo Kalil <rkbuarque@gmail.com>
2024-12-24 02:08:39 -0300

    feat: update items and survivors services

--- app/services/items_service.ts
+++ app/services/items_service.ts
@@ -1,5 +1,6 @@
 import { ItemInterface } from '../interfaces/item_interface.ts'
 import Item from '../models/items.ts'
+import ItemPoints from '../models/items_points.ts'
 import Survivor from '../models/survivors.ts'
 
 export default class ItemsService {
@@ -24,5 +25,29 @@ export default class ItemsService {
     })
 
     return items
+  }
+
+  async validateItems(listToValidate: any[]) {
+    /**
+     * compare if the items received are different from the items on items_points table
+     */
+    const itemsPoints = await ItemPoints.all()
+
+    const set = new Set(itemsPoints.map((item) => item['name']))
+
+    const isSubset = listToValidate.every((item) => set.has(item['name']))
+
+    return isSubset
+
+    // const itemsPoints = await ItemPoints.all()
+    // const isSubset = listToValidate.every((item2) =>
+    //   itemsPoints.some((item1) => item1['name'] === item2['name'])
+    // ) compare performance
+  }
+
+  async hasUniqueItems(items: ItemInterface[]) {
+    const uniqueNames = new Set(items.map((item) => item.name))
+
+    return uniqueNames.size === items.length
   }
 }
--- app/services/survivors_service.ts
+++ app/services/survivors_service.ts
@@ -2,22 +2,25 @@ import { inject } from '@adonisjs/core'
 import { SurvivorInterface, UpdateLocationInterface } from '../interfaces/survivor_interface.ts'
 import Survivor from '../models/survivors.ts'
 import ItemsService from './items_service.ts'
-import ItemPoints from '../models/items_points.ts'
+import { ItemMessages } from '../messages/item_message.ts'
 
 @inject()
 export default class SurvivorsService {
   constructor(protected itemsService: ItemsService) {}
 
   async create(payload: SurvivorInterface) {
-    /**
-     * compare if the items received are different types of items points table
-     */
-    const itemsPoints = await ItemPoints.all()
-    const isSubset = payload.items.every((item2) =>
-      itemsPoints.some((item1) => item1['name'] === item2['name'])
-    )
+    // add transactions
+
+    const hasUniqueItems = await this.itemsService.hasUniqueItems(payload.items)
+
+    if (!hasUniqueItems) {
+      throw new Error('Duplicated items not allowed')
+    }
+
+    const isSubset = await this.itemsService.validateItems(payload.items)
+
     if (!isSubset) {
-      throw new Error('one of the items received is not included in the item points table')
+      throw new Error(ItemMessages.ITEM_NOT_VALID)
     }
 
     const survivor = await Survivor.findBy({ name: payload.name })
@@ -37,5 +40,23 @@ export default class SurvivorsService {
     await survivor.merge({ lastLocation }).save()
   }
 
-  async reportContamination() {}
+  async reportContamination(id: number) {
+    const survivor = await Survivor.findByOrFail({ id: id })
+
+    const maxReportsCount = 5
+
+    if (survivor.infectedFlagCount < maxReportsCount) {
+      const count = survivor.infectedFlagCount + 1
+
+      survivor.merge({ infectedFlagCount: count }).save()
+
+      if (count >= maxReportsCount) {
+        survivor.merge({ isInfected: true }).save()
+      }
+    }
+  }
+
+  async kill() {}
+
+  async loot() {}
 }
